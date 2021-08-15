e6e91362ceceeb2379c94c8f6848e809c777e98a
iggyi <iccyn@pm.me>
2021-08-15 18:04:25 +1000

    added wave

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <a href="https://github.com/austintgriffith/scaffold-eth/tree/uniswapper" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Uniswap swap interface"
+        title="🍣 HackAfrica X Sushiswap"
+        subTitle="Challenge 1: Sushiswap Interface"
         style={{ cursor: "pointer" }}
       />
     </a>
